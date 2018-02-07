a1ac327779df42de8e102602981b91c7b5a08a9a
JD Pawar <jd.pawar@enuma.io>
2018-02-07 16:30:22 +0800

    update: UI, get BTC and ETH address via button click

--- js/index.js
+++ js/index.js
@@ -5,6 +5,12 @@ function verifyPackage() {
     console.log(AppEth.default)
 }
 
+verifyPackage()
+
+
+const btcWalletPath = "44'/0'/0'/0"
+const ethWalletPath = "44'/60'/0'/0'/0"
+
 
 const getBtcAddress = async (path) => {
 
@@ -12,25 +18,55 @@ const getBtcAddress = async (path) => {
 
     if (devices.length === 0) throw "no device connected"
 
-    // return devices[0]
     const transport = await Transport.default.open(devices[0])
     const btc = new AppBtc.default(transport)
     const result = await btc.getWalletPublicKey(path)
     return result.bitcoinAddress
 }
 
-verifyPackage()
 
-const path = "44'/0'/0'/0"
+const getEthAddress = async (path) => {
 
-getBtcAddress(path)
-    .then(result => {
+    const devices = await Transport.default.list()
 
-        console.log(result)
-        document.getElementById("result").innerHTML = JSON.stringify(result)
-    })
-    .catch(error => {
+    if (devices.length === 0) throw "no devices connected"
 
-        console.error(error)
-        document.getElementById("result").innerHTML = JSON.stringify(error)
-    })
+    const transport = await Transport.default.open(devices[0])
+    const eth = new AppEth.default(transport)
+    const result = await eth.getAddress(path, true, true)
+    return result.address
+}
+
+
+function onGetBtcAddress() {
+
+    getBtcAddress(btcWalletPath)
+        .then(result => {
+
+            console.log(result)
+            document.getElementById("result").innerHTML = JSON.stringify(result)
+        })
+        .catch(error => {
+
+            console.error(error)
+            document.getElementById("result").innerHTML = JSON.stringify(error)
+        })
+
+}
+
+
+function onGetEthAddress() {
+
+    getEthAddress(ethWalletPath)
+        .then(result => {
+
+            console.log(result)
+            document.getElementById("result").innerHTML = JSON.stringify(result)
+        })
+        .catch(error => {
+
+            console.error(error)
+            document.getElementById("result").innerHTML = JSON.stringify(error)
+        })
+
+}
