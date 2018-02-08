2cbbb34066c2baca4f92a8c12763e1cc374562de
JD Pawar <jd.pawar@enuma.io>
2018-02-08 12:52:00 +0800

    test 04, send message back from external popup

--- js/index.js
+++ js/index.js
@@ -60,6 +60,7 @@ function onGetBtcAddress(btcPath) {
 
             var event = document.createEvent("Event")
             event.initEvent("errorBtcAddress")
+            const _errorBtcAddressMessage = error
             document.dispatchEvent(event)
             // chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
             //     chrome.tabs.sendMessage(tabs[0].id, error, function(response) {
